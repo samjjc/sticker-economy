7565006f7134ef130bab5b7970e1d303400ea956
samjjc <sam111jc@gmail.com>
2018-03-01 16:19:01 -0500

    made first model

--- economy/models.py
+++ economy/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Sticker(models.Model):
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
