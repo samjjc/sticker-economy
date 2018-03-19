5ef05e85c6ab44e265319b8b1ccc6fafa851b93b
samjjc <sam111jc@gmail.com>
2018-03-19 14:16:38 -0400

    remove unused method

--- economy/models.py
+++ economy/models.py
@@ -26,10 +26,6 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
 
-
-    def __unicode__(self):
-        return self.user.username
-
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
     if created:
