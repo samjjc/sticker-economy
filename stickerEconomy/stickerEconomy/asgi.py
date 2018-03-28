ea6cfb1c2f0cafb504d4e3579e9952db3057641f
samjjc <sam111jc@gmail.com>
2018-03-27 22:42:39 -0400

    add daphne asgi config

--- stickerEconomy/stickerEconomy/asgi.py
+++ stickerEconomy/stickerEconomy/asgi.py
@@ -4,9 +4,17 @@ defined in the ASGI_APPLICATION setting.
 """
 
 import os
-import django
-from channels.routing import get_default_application
+from channels.asgi import get_channel_layer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stickerEconomy.settings")
-django.setup()
-application = get_default_application()
+
+channel_layer = get_channel_layer()
+application = get_channel_layer()
+
+# import os
+# import django
+# from channels.routing import get_default_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stickerEconomy.settings")
+# django.setup()
+# application = get_default_application()
